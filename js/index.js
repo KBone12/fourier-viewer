5a8ae9aa2b43852f94414c9b809f6e3d7aa1ed50
kbone <kbonehobby@gmail.com>
2020-11-29 15:39:37 +0900

    Show spectra

--- js/index.js
+++ js/index.js
@@ -7,8 +7,15 @@ import("../pkg/index.js").then(rust => {
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     files[0].arrayBuffer().then(buffer => audioContext.decodeAudioData(buffer)).then(data => {
       console.log(data);
-      const spectra = rust.run_fft(data.getChannelData(0), 8192);
-      console.log(spectra);
+      const fftSize = 1 << 16;
+      const spectra = rust.run_fft(data.getChannelData(0), fftSize);
+      const powers = rust.spectra_to_powers(spectra);
+      const peak_values = document.getElementById("peak_values");
+      const peak_indices = rust.peak_indices(powers.slice(0, powers.length / 2), 5);
+      for (let i = 0; i < peak_indices.length; i += 1) {
+        peak_values.innerText += "" + peak_indices[i] * (data.sampleRate / fftSize) + ", ";
+      }
+      rust.plot_power_spectra_to_canvas(document.getElementById("canvas"), powers, data.sampleRate);
     }).catch(console.error).finally(() => {
       audioContext.close();
     });
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
+use plotters::{
+    chart::ChartBuilder,
+    drawing::IntoDrawingArea,
+    series::LineSeries,
+    style::{
+        colors::{BLUE, WHITE},
+        Color,
+    },
+};
+use plotters_canvas::CanvasBackend;
+
 use rustfft::{num_complex::Complex, num_traits::Zero, FFTplanner};
 
 use wasm_bindgen::prelude::wasm_bindgen;
+
+use web_sys::HtmlCanvasElement;
 
 #[wasm_bindgen]
 pub fn run_fft(input: &[f32], size: usize) -> Vec<f32> {
@@ -13,4 +26,67 @@ pub fn run_fft(input: &[f32], size: usize) -> Vec<f32> {
     let mut output = vec![Complex::zero(); size];
     fft.process(&mut input, &mut output);
     output.iter().flat_map(|c| vec![c.re, c.im]).collect()
+}
+
+#[wasm_bindgen]
+pub fn spectra_to_powers(spectra: &[f32]) -> Vec<f32> {
+    spectra
+        .chunks_exact(2)
+        .map(|chunk| chunk[0] * chunk[0] + chunk[1] * chunk[1])
+        .collect()
+}
+
+#[wasm_bindgen]
+pub fn peak_indices(powers: &[f32], num: usize) -> Vec<usize> {
+    let mut tmp: Vec<_> = powers.iter().enumerate().collect();
+    tmp.sort_by(|(_, a), (_, b)| b.partial_cmp(a).expect("Contains NaN"));
+    tmp[..num].iter().map(|(i, _)| *i).collect()
+}
+
+#[wasm_bindgen]
+pub fn plot_power_spectra_to_canvas(canvas: HtmlCanvasElement, powers: &[f32], sample_rate: f32) {
+    let root = CanvasBackend::with_canvas_object(canvas)
+        .expect("Illegal canvas")
+        .into_drawing_area();
+    root.fill(&WHITE)
+        .expect("Some errors have been occurred in the backend");
+    let df = sample_rate / powers.len() as f32;
+    let mut chart = ChartBuilder::on(&root)
+        .x_label_area_size(50)
+        .y_label_area_size(60)
+        .build_cartesian_2d(
+            0..powers.len() / 2,
+            0.0..plotters::data::fitting_range(powers).end + 0.1,
+        )
+        .expect("Can't build a 2d Cartesian coordinate");
+    chart
+        .configure_mesh()
+        .x_labels(10)
+        .y_labels(10)
+        .disable_mesh()
+        .x_label_formatter(&|&v| {
+            format!(
+                "{:0.1}",
+                if v < (powers.len() + 1) / 2 {
+                    v as f32 * df
+                } else {
+                    -((powers.len() - v) as f32) * df
+                }
+            )
+        })
+        .y_label_formatter(&|v| format!("{:e}", v))
+        .x_desc("Frequency [Hz]")
+        .y_desc("Power")
+        .draw()
+        .expect("Can't draw axes");
+    chart
+        .draw_series(LineSeries::new(
+            powers
+                .iter()
+                .take(powers.len() / 2)
+                .enumerate()
+                .map(|(i, p)| (i, *p)),
+            BLUE.filled(),
+        ))
+        .expect("Can't draw a series");
 }
