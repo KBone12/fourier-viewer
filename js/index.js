92a7f76ff1e050b825ef209021fae799ca071879
kbone <kbonehobby@gmail.com>
2020-11-29 12:37:20 +0900

    Run FFT

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import("../pkg/index.js").then(_module => {
+import("../pkg/index.js").then(rust => {
   document.getElementById("file").addEventListener("change", function () {
     const files = this.files;
     if (files.length === 0) {
@@ -7,6 +7,8 @@ import("../pkg/index.js").then(_module => {
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     files[0].arrayBuffer().then(buffer => audioContext.decodeAudioData(buffer)).then(data => {
       console.log(data);
+      const spectra = rust.run_fft(data.getChannelData(0), 8192);
+      console.log(spectra);
     }).catch(console.error).finally(() => {
       audioContext.close();
     });
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
+use rustfft::{num_complex::Complex, num_traits::Zero, FFTplanner};
+
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-pub fn helloworld() {}
+pub fn run_fft(input: &[f32], size: usize) -> Vec<f32> {
+    let mut planner = FFTplanner::<f32>::new(false);
+    let fft = planner.plan_fft(size);
+
+    let mut input = input.to_vec();
+    input.resize(size, 0.0);
+    let mut input: Vec<_> = input.iter().map(|f| Complex::new(*f, 0.0)).collect();
+    let mut output = vec![Complex::zero(); size];
+    fft.process(&mut input, &mut output);
+    output.iter().flat_map(|c| vec![c.re, c.im]).collect()
+}
