ce9a7313ad408a2777ad76d3f998721910f38efc
kbone <kbonehobby@gmail.com>
2020-11-29 12:08:28 +0900

    Input a file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-import("../pkg/index.js").catch(console.error);
+import("../pkg/index.js").then(_module => {
+  document.getElementById("file").addEventListener("change", function () {
+    const files = this.files;
+    if (files.length === 0) {
+      return;
+    }
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    files[0].arrayBuffer().then(buffer => audioContext.decodeAudioData(buffer)).then(data => {
+      console.log(data);
+    }).catch(console.error).finally(() => {
+      audioContext.close();
+    });
+  }, false);
+}).catch(console.error);
